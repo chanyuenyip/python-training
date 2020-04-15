de05635241682ec0086b72ec1bf573da27b489fa
Rex.ChanYuenYip <dev.yuenyip@gmail.com>
2020-04-15 15:10:25 +0800

    Add files via upload

---
+++ session11_assign.py
@@ -0,0 +1,12 @@
+#1
+availalbe = 100
+attendee = ['a','b','c','d','e','f','g']
+remaining = availalbe - len(attendee)
+print(remaining)
+
+#2
+n = [1,2,3,4,5,6,7,8,9,10]
+product = 1
+for num in n:
+	product = product * num
+print(product)
